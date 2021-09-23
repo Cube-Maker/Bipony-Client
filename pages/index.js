4765823be39ba11d859e0bd1b22e4593f2539f1a
Imtious-coder <iarafat386@gmail.com>
2021-09-23 07:25:51 +0600

    Products Deleted

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Header from "../src/layout/Header";
-<<<<<<< HEAD
-=======
 import Banner from "../src/view/HomePage/component/Banner/Banner";
->>>>>>> Backup
 
 export const getStaticProps = async () => {
   const res = await fetch("https://fakestoreapi.com/products");
@@ -17,15 +14,11 @@ export default function Home({ products }) {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
-      <HomePage products={products} />
-=======
       <Banner />
-      <HomePage products={products} />
+      {/* <HomePage products={products} /> */}
 
->>>>>>> Backup
     </div>
   );
 }
 
-import HomePage from "../src/view/HomePage/HomePage";
+// import HomePage from "../src/view/HomePage/HomePage";
