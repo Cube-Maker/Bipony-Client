7a8091195fd4b8a88a56ec58337a850a916886d5
Imtious-coder <iarafat386@gmail.com>
2021-09-23 15:07:47 +0600

    Updated

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     return (
         <div>
             <Header />
-                {children}
+            {children}
             <Footer />
         </div>
     )
