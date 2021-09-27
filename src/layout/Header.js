387032765e26d25e968e874108db4f13f7905516
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2021-09-27 10:27:54 +0600

    grid update 6 to 5

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./Header.module.css";
 
-<<<<<<< HEAD
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartPlus, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartPlus,
+  faUserCircle,
+  faSignInAlt,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
@@ -17,7 +21,9 @@ export default function Header() {
               placeholder="Search Product"
               className={styles.searchInput}
             />
-            <input type="submit" className={styles.searchBtn} />
+            <button className={styles.searchBtn}>
+              <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
+            </button>
           </form>
         </div>
         <div className={styles.headerDetails}>
@@ -28,42 +34,10 @@ export default function Header() {
             <FontAwesomeIcon className={styles.icon} icon={faCartPlus} />
           </div>
           <div className={styles.headerLogin}>
-            <button className={styles.loginBtn}>LogIn</button>
+            <FontAwesomeIcon className={styles.icon} icon={faSignInAlt} />
           </div>
         </div>
       </div>
     </header>
   );
-=======
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus, faUserCircle, faSignInAlt, faSearch } from '@fortawesome/free-solid-svg-icons'
-
-export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.grid}>
-                <div className={styles.logoContainer}>
-                    Bipony
-                </div>
-                <div className={styles.headerSearch}>
-                    <form >
-                        <input type="text" placeholder="Search Product" className={styles.searchInput} />
-                        <button className={styles.searchBtn}><FontAwesomeIcon className={styles.searchIcon} icon={faSearch} /></button>
-                    </form>
-                </div>
-                <div className={styles.headerDetails}>
-                    <div className={styles.headerProfile}>
-                        <FontAwesomeIcon className={styles.icon} icon={faUserCircle} />
-                    </div>
-                    <div className={styles.headerCart}>
-                        <FontAwesomeIcon className={styles.icon} icon={faCartPlus} />
-                    </div>
-                    <div className={styles.headerLogin}>
-                        <FontAwesomeIcon className={styles.icon} icon={faSignInAlt} />
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
->>>>>>> a0ef8c8e443f742927e34ec0e82522c7adca57fa
 }
