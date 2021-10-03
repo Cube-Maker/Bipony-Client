e5e76edb2c34443ead8cd5d2ac461b9a7af899a2
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-10-03 15:49:42 +0600

    upload seller dashboard and seller info

--- src/view/ShopPage/ShopPage.js
+++ src/view/ShopPage/ShopPage.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ShopDetails from './component/ShopDetails/ShopDetails'
 import Sidebar from './component/Sidebar/Sidebar'
 
 import styles from './ShopPage.module.css'
 
 export default function ShopPage() {
+    const [link,setLink] = useState(1)
     return (
         <div className={styles.shopPage}>
-            <Sidebar />
-            <ShopDetails />
+            <Sidebar setLink={setLink} />
+            <ShopDetails link = {link}/>
         </div>
     )
 }
---
+++ src/view/SwitchToSellerPage/SwitchToSellerPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SellerSignUp from './component/SellerSignUp/SellerSignUp'
+
+export default function SwitchToSellerPage() {
+    return (
+        <>
+            <SellerSignUp />
+        </>
+    )
+}
