dd614c1df1348a398b7a65a7a028483ce8aeeef6
Imtious-coder <iarafat386@gmail.com>
2021-10-12 11:37:52 +0600

    puzzle system added

---
+++ pages/Puzzle/PuzzleSolve/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PuzzleSolve from '../../../src/view/LoginSignup/Puzzle/PuzzleSolve/PuzzleSolve';
+
+const index = () => {
+    return (
+        <>
+            <PuzzleSolve />
+        </>
+    );
+};
+
+export default index;
---
+++ pages/Puzzle/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Puzzle from '../../src/view/LoginSignup/Puzzle/Puzzle';
+
+const index = () => {
+    return (
+        <>
+            <Puzzle />
+        </>
+    );
+};
+
+export default index;
