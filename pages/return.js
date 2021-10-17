0846db5fb7f1941e1d4521643b560192b139b1a8
Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
2021-10-18 01:00:36 +0600

    Squashed commit of the following:
    commit 3fb225a463b487140cf96a82b7db7b0e955513ac
    Author: Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
    Date:   Mon Oct 18 00:56:48 2021 +0600
    
        Banner Style Modified
    
    commit 2b88ef3fd1e3bde70e9cf3aa99ddbfcccb474523
    Merge: 29011b7 8901a63
    Author: Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
    Date:   Mon Oct 18 00:38:55 2021 +0600
    
        Merge branch 'main' into Sakib
    
    commit 29011b7ef7fff867b62315b171bdafdf6af54f8c
    Author: Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
    Date:   Mon Oct 18 00:09:10 2021 +0600
    
        Solved production tailwind problem
    
    commit cca928cf99ec3d446f7a6b1a736dbe66896aa974
    Author: Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
    Date:   Wed Oct 13 00:27:26 2021 +0600
    
        Return page slightly polished
    
    commit 5cb03a0626295a7597e251e3cc62837f872eddff
    Author: Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
    Date:   Wed Oct 13 00:24:04 2021 +0600
    
        Product return page designed

---
+++ pages/return.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../src/layout/Layout";
+import ProductReturnPage from '../src/view/ProductReturnPage';
+
+
+export default function Return() {
+    return (
+        <Layout>
+           <ProductReturnPage/>
+        </Layout>
+    )
+}
---
+++ src/view/ProductReturnPage/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const ProductReturnPage = () => {
+    return (
+        <div className="w-full bg-gray-200">
+            <h1 className="text-4xl leading-none">Product Return</h1>
+            <section className="mx-40">
+                <div className="leading-loose">
+                    <form className="max-w-4xl p-10 bg-white rounded shadow-xl">
+                        <p className="text-gray-800 font-medium">Customer information</p>
+                        <div className="">
+                            <label className="block text-sm text-gray-00" >Name</label>
+                            <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required placeholder="Your Name" aria-label="Name" />
+                        </div>
+                        <div className="mt-2">
+                            <label className="block text-sm text-gray-600">Email</label>
+                            <input className="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="email" required placeholder="Your Email" aria-label="Email" />
+                        </div>
+                        <div className="mt-2">
+                            <label className=" block text-sm text-gray-600">Address</label>
+                            <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required placeholder="Street" aria-label="Address" />
+                        </div>
+                        <div className="mt-2">
+                            <label className="hidden text-sm block text-gray-600">City</label>
+                            <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required placeholder="City" aria-label="City" />
+                        </div>
+                        <div className="inline-block mt-2 w-1/2 pr-1">
+                            <label className="hidden block text-sm text-gray-600">Country</label>
+                            <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required placeholder="Country" aria-label="Country" />
+                        </div>
+                        <div className="inline-block mt-2 -mx-1 pl-1 w-1/2">
+                            <label className="hidden block text-sm text-gray-600">Zip</label>
+                            <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required placeholder="Zip" aria-label="Zip" />
+                        </div>
+                        <p className="mt-2 text-gray-800 font-medium">Order information</p>
+                        <div className="">
+                            <label className="block text-sm text-gray-600">Order ID</label>
+                            <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required placeholder="Enter order ID here" aria-label="Order ID" />
+                        </div>
+                        <div className="mt-2">
+                            <label className="block text-sm text-gray-600">Why you returning this Product</label>
+                            <input className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="textarea" required placeholder="Your Problem" aria-label="Problem" />
+                        </div>
+                        <div className="mt-4">
+                            <button className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default ProductReturnPage;
